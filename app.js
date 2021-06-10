03efe9f4e33fbeea16862db919903223cc9eb374
Santiago Ramirez <santi_coding@outlook.com>
2021-06-09 21:07:15 -0400

    Starting this covid survey with frontend ready

---
+++ app.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const expHandleB = require("express-handlebars");
+const nodemailer = require("nodemailer");
+
+const app = express();
+
